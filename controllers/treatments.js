9554b2982e4b4df23aad105030c0ba987d672ad4
MarkKorcz <mark.korcz@gmail.com>
2021-01-12 21:29:08 +0100

    adding few pages

---
+++ controllers/treatments.js
@@ -0,0 +1,15 @@
+exports.main = (req, res) =>{
+    res.render('treatments/main')
+}
+
+exports.dermapen = (req, res) =>{
+    res.render('treatments/dermapen')
+}
+
+exports.peeling = (req, res) =>{
+    res.render('treatments/peeling')
+}
+
+exports.oxygenInfusion = (req, res) =>{
+    res.render('treatments/oxygen-infusion')
+}
--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const mainController = require('../controllers/main')
 
-router.get('/', mainController.main)
+const homeController = require('../controllers/home')
+router.get('/', homeController.home)
+
+const treatmentsController = require('../controllers/treatments')
+router.get('/treatments', treatmentsController.main)
+router.get('/dermapen', treatmentsController.dermapen)
+router.get('/peeling', treatmentsController.peeling)
+router.get('/oxygen-infusion', treatmentsController.oxygenInfusion)
 
 module.exports = router
